e5f52b9b00b596c6a299b6520ebb92d901549edd
James Johnson <jamesjhnsn1995@gmail.com>
2022-01-23 12:08:09 -0800

    fixed image url

--- library_app/management/commands/populate_db.py
+++ library_app/management/commands/populate_db.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 import requests, json
 
 
+
 from library_app.models import Book, Category, Author
 
 class Command(BaseCommand):
@@ -10,65 +11,74 @@ class Command(BaseCommand):
         Book.objects.all().delete()
         Category.objects.all().delete()
         Author.objects.all().delete()
-        test = f'https://covers.openlibrary.org/b/$dracula/$value-$size.jpg'
-        # image_query = requests.get(test)
-        # img = image_query.json()#.get('query')
-        print(test)
         
         
-        # def query(title):
-        #     if '; ' in title:
-        #         summary = title.split('; ')[0]
-        #         # print(summary)
-        #         url2 = f"https://en.wikipedia.org/api/rest_v1/page/summary/{summary}"
-        #         response2 = requests.get(url2)
-        #         summary = response2.json().get('extract')
-        #     elif ' ' or ',' or '-' in title:
-        #         summary = title.replace(' ', '_').replace(',', '').replace('-', '')
-        #         # print(summary)
-        #         url2 = f"https://en.wikipedia.org/api/rest_v1/page/summary/{summary}"
-        #         response2 = requests.get(url2)
-        #         summary = response2.json().get('extract')
-        #     return summary
+        def query(title):
+            if '; ' in title:
+                summary = title.split('; ')[0]
+                # print(summary)
+                url2 = f"https://en.wikipedia.org/api/rest_v1/page/summary/{summary}"
+                response2 = requests.get(url2)
+                summary = response2.json().get('extract')
+                if summary == None:
+                    summary = 'No description available'
+            elif ' ' or ',' or '-' in title:
+                summary = title.replace(' ', '_').replace(',', '').replace('-', '')
+                # print(summary)
+                url2 = f"https://en.wikipedia.org/api/rest_v1/page/summary/{summary}"
+                response2 = requests.get(url2)
+                summary = response2.json().get('extract')
+                if summary == None:
+                    summary = 'No description available'
+            return summary
 
-        # def category_query(category):
-        #     if '--' in category:
-        #         category = category.replace('--', '')
-        #     else:
-        #         category = category
-        #     return category
+        def category_query(category):
+            if '--' in category:
+                category = category.replace('--', '')
+            else:
+                category = category
+            return category
         
         
-        # loop = 1
+        def image_search(image_url):
+            for data in image_url:
+                if '.cover.medium.jpg' in data['uri']:
+                    image_url = data['uri']
+                    print(image_url)
+            return image_url
         
-        # while loop < 2:
-        #     loop_str = str(loop)
-        #     url = f"https://gnikdroy.pythonanywhere.com/api/book/?format=json&page={loop_str}"
-        #     response = requests.get(url)
-        #     books = response.json().get('results')
+        loop = 1
+        
+        while loop < 2:
+            loop_str = str(loop)
+            url = f"https://gnikdroy.pythonanywhere.com/api/book/?format=json&page={loop_str}"
+            response = requests.get(url)
+            books = response.json().get('results')
             
-        #     for book in books:
-        #         title = book['title']
-        #         image_url = book['resources'][3]['uri']
-        #         category = book['subjects']
-        #         description = query(title)
-        #         author = book['agents'][0]['person']
-        #         # print(title)
+            
+            for book in books:
+                title = book['title']
+                image_url = book['resources']
+                image_url = image_search(image_url)
+                category = book['subjects']
+                description = query(title)
+                author = book['agents'][0]['person']
+                print(type(image_url[0]))
                 
 
                 
-        #         book = Book.objects.create(
-        #             title = title,
-        #             image_url = image_url,
-        #             description = description,
+                book = Book.objects.create(
+                    title = title,
+                    image_url = image_url,
+                    description = description,
                     
-        #         )
+                )
 
-        #         author, created = Author.objects.get_or_create(name=author)
-        #         author.books.add(book)
+                author, created = Author.objects.get_or_create(name=author)
+                author.books.add(book)
                 
-        #         for category in map(str, category):
-        #             category = category_query(category)
-        #             category, created = Category.objects.get_or_create(title=category)
-        #             book.category.add(category)
-        #     loop += 1
+                for category in map(str, category):
+                    category = category_query(category)
+                    category, created = Category.objects.get_or_create(title=category)
+                    book.category.add(category)
+            loop += 1
