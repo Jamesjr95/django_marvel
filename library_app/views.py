879d5903c11e321eeb6b0d880d5612895a845ed1
James Johnson <jamesjhnsn1995@gmail.com>
2022-07-25 14:16:29 -0700

    cleaned up views

--- library_app/views.py
+++ library_app/views.py
@@ -6,126 +6,103 @@ from .models import Book, Author, Character, Checkout, CheckoutItem
 from django.db.models import Q
 from django.contrib.auth import get_user_model
 from django.utils import timezone
-# from django.db import connection, reset_queries
-import random
 import requests
 from django.contrib import messages 
 from django.core.paginator import Paginator
-# Create your views here.
 
-
+#list view for all comic books
 def index(request, page_num=1, per_page=24):
-    
     page_num = request.GET.get('page_num') or page_num
     per_page = request.GET.get('per_page') or per_page
-
     books = Book.objects.all()
     books = books.order_by('-date')
-    
     products_page = Paginator(books, per_page).get_page(page_num)
-    
     context = {
         'products_page': products_page
 
     }
-
     return render(request, 'catalog/index.html', context)
 
+#list view for all characters
 def characters(request,  page_num=1, per_page=24):
     page_num = request.GET.get('page_num') or page_num
     per_page = request.GET.get('per_page') or per_page
     characters = Character.objects.all()
-    
     products_page = Paginator(characters, per_page).get_page(page_num)
-    
     context = {
         'products_page': products_page
     }
     return render(request, 'catalog/characters.html', context)
 
+#add item to logged in user's cart
 @login_required
 def add_to_cart(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     checkout_item = CheckoutItem.objects.all()
-
     checkout = Checkout.objects.get(owner=request.user)
     today = timezone.now()
     today_time = today.strftime("%m/%d/%Y, %H:%M:%S")
     due_date = today + timezone.timedelta(days=7)
-    print(today_time)
-
-    
     checkout_item, created = CheckoutItem.objects.get_or_create(
         book=book, checkout=checkout)
-
     checkout_item.quantity += 1
     checkout_item.due_date = due_date
     checkout_item.save()
     messages.success(request, "Cart updated!", extra_tags='success')
-    
     return redirect(reverse('library_app:index'))
 
+#update logged in user's cart
 @login_required
 def update_cart(request, checkout_item_id):
     checkout_item = get_object_or_404(CheckoutItem, id=checkout_item_id)
-    
     new_quantity = request.POST.get('quantity')
-    
     checkout_item.quantity = new_quantity
-    
     checkout_item.save()
-    
     return redirect(reverse('users_app:profile', kwargs= {'username': request.user.username}))
 
+#delete item from logged in user's cart
 @login_required
 def remove_from_cart(request, checkout_item_id):
     checkout_item = get_object_or_404(CheckoutItem, id=checkout_item_id)
     checkout_item.delete()
     return redirect(reverse('users_app:profile', kwargs={'username': request.user.username}))
 
-
+#detail view for a book
 def detail(request, book_id):
-
     books = get_object_or_404(Book, id=book_id)
     author = Author.objects.filter(books=books)[0]
-    
     context = {
         'books': books,
         'author': author,
     }
     return render(request, 'catalog/details.html', context)
 
+#view to toggle if a user logged in user likes a book
 @login_required
 def like(request, book_id):
     book = get_object_or_404(Book, id=book_id)
-    print(book)
-
     if request.user not in book.likes.all():
         book.likes.add(request.user)
     else:
         book.likes.remove(request.user)
-    print(book_id)
     return JsonResponse({
 
         'isLiked': request.user in book.likes.all(),
         'likeCount': book.likes.count()
     })
 
-
+#detail view for a character
 def character(request, character_id):
-
     character = get_object_or_404(Character, id=character_id)
-  
     context = {
         'character': character,
     }
     return render(request, 'catalog/character.html', context)
 
+#list view for a search 
 def search_query(request):
     books = Book.objects.all()
-   
     search_query = request.POST.get('search-query')
-    
     if search_query:
         search_query = books.filter(
             Q(title__icontains=search_query)
@@ -135,5 +112,4 @@ def search_query(request):
     context = {
         'search_query': search_query,
     }
-
     return render(request, 'catalog/search_query.html', context)
--- users_app/views.py
+++ users_app/views.py
@@ -1,50 +1,33 @@
 from django.shortcuts import render
 
-# Create your views here.
 from django.http.response import JsonResponse
 from django.shortcuts import get_object_or_404, render, redirect, reverse
 from django.contrib.auth import (
-    get_user_model, # returns the AUTH_USER_MODEL variable's value from settings.py
+    get_user_model,
     authenticate,
     login as django_login,
     logout as django_logout
 )
 from django.contrib.auth.decorators import login_required
-
 from .forms import UserForm, UserAuthForm
 from library_app.models import Checkout
 
+#sign up view
 def register(request):
-
     form = UserAuthForm()
     if request.method == 'GET':
         context = {
             'form': form
         }
-
         return render(request, 'users/register.html', context)
     if request.method == 'POST':
-        # create a UserAuthForm with the data from the HTML form
         form = UserAuthForm(request.POST)
-
-        # print(form.is_valid())
-        # print(form.errors)
-
         if form.is_valid():
-            # commit=False will create the object but won't save it
             new_user = form.save(commit=False)
-
-            # set the new user's password
-            # validated form data is in form.cleaned_data
             new_user.set_password(form.cleaned_data['password'])
-
-            # save the object to the database
             new_user.save()
-
             checkout = Checkout.objects.create(owner=new_user)
-            # redirect() does the same thing as HttpResponseRedirect
             return redirect(reverse('users_app:register'))
-
         else:
             context = {
                 'form': UserAuthForm(),
@@ -53,39 +36,32 @@ def register(request):
 
             return render(request, 'users/register.html', context)
 
+#login view
 def login(request):
     if request.method == 'GET':
-
         form = UserAuthForm()
         return render(request, 'users/login.html', {'form': form})
-
     elif request.method == 'POST':
-
         form = request.POST
         username = form['username']
         password = form['password']
-
         user = authenticate(request, username=username, password=password)
-
         if user is None:
             context = {
                 'form': UserAuthForm(),
                 'errors': ['Invalid Username or Password']
             }
-
             return render(request, 'users/login.html', context)
-
-
         else:
             django_login(request, user)
             return redirect(reverse('users_app:profile', kwargs={'username': user.username}))
 
+#detail profile view
 def profile(request, username):
     user = get_object_or_404(get_user_model(), username=username)
     return render(request, 'users/profile.html', {'user': user})
-    
 
-
+#logout view
 def logout(request):
     django_logout(request)
 
