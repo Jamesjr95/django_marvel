e81fd3d11c32d96d702731e02f8845492972da63
James Johnson <jamesjhnsn1995@gmail.com>
2022-10-11 18:25:47 -0700

    styling frontend

--- users_app/forms.py
+++ users_app/forms.py
@@ -14,8 +14,8 @@ class UserForm(forms.ModelForm):
         widgets = {
             'first_name': forms.TextInput(attrs={'class':'form-control'}),
             'last_name': forms.TextInput(attrs={'class':'form-control'}),
-            'username': forms.TextInput(attrs={'class':'form-control', 'id':'log_button'}),
-            'password': forms.PasswordInput(attrs={'class':'form-control', 'id':'log_button'}),
+            'username': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Username', 'style':'font-style:italic'}),
+            'password': forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Password', 'style':'font-style:italic'}),
         }
 
 class UserAuthForm(UserForm):
